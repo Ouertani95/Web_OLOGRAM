d2b97f77804da274ce4b2cbe95ee6b3e8f5fd7a0
Mohamed <mohamed.ouertani.1@etu.univ-amu.fr>
2022-04-22 16:29:11 +0200

    new validation process  + dynamic command with input files  + new display function

--- test-app/app/Http/Controllers/JobsController.php
+++ test-app/app/Http/Controllers/JobsController.php
@@ -14,8 +14,16 @@ class JobsController extends Controller
         return view('main');
     }
 
-    public function run_job ()
+    public function run_job (Request $request)
     {   
+        // Validate form fields
+        $validated = $request->validate([
+            'email' => 'bail|required|email|max:100',
+            'gtf' => 'required',
+            'bed' => 'required',
+            'chr' => 'required'
+        ]);
+
         // Add form information  to Jobs database using Job model
         Job::create([
             'email' => request('email'),
@@ -24,19 +32,53 @@ class JobsController extends Controller
             'chr' => request('chr')
         ]);
         
-        $command = "docker exec gtftk conda run -n pygtftk gtftk ologram -i mini_real.gtf.gz -c hg38.genome -p ENCFF112BHN_H3K4me3_K562_sub.bed -o test3.pdf 2>&1";
+        // Get uploaded files
+        $gtf = $request->file('gtf')->getClientOriginalName();
+        $bed = $request->file('bed')->getClientOriginalName();
+        $chr = $request->file('chr')->getClientOriginalName();
+
+        // Prepare date and directory variables
+        $date = date("Ymd_His",time());
+        $directory_name = $date.'-'.$gtf.'-'.$bed;
+        
+        // Initialise command variables
+        $command = "docker exec gtftk conda run -n pygtftk gtftk ologram -i $gtf -c $chr -p $bed -o $directory_name 2>&1" ;
         $output=null;
         $return_var=null;
+
         // Execute shell command in php
         exec($command, $output, $return_var);
-        // Echo the output and the error code
-        echo "Returned with status $return_var and output:\n";
-        print_r($output);
 
+        // Verify if errors occured during execution of command
+        if ($return_var!== 0) {
+
+            // If there are errors display the output of the error
+            return back()->with('error','<pre>' . print_r($output,$return=true) . '</pre>');
+        }
+        
+        // If no errors display result
+        else{
+            $result = $this->display_result($directory_name);
+            return response()->file($result);
+        }
+    
     }
 
     public function show_message()
     {
         return redirect()->to('/main')->with('success', 'Job is running !');
+    }
+
+    public function display_result($directory)
+    {
+        $files = scandir("../pygtftk_results/".$directory);
+        foreach ($files as $file){
+            if (str_contains($file,"pdf")){
+                $file_path = "../pygtftk_results/".$directory.'/'.$file;
+                    return $file_path;
+            }
+        }
+        
+        
     }
 }
