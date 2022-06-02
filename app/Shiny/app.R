382d011adbcd2b8a1dc1f790dde41857dd1e9280
Mohamed <mohamed.ouertani.1@etu.univ-amu.fr>
2022-06-02 16:22:39 +0200

    added feature selection input

--- app/Shiny/app.R
+++ app/Shiny/app.R
@@ -87,7 +87,7 @@ ui <- fluidPage(
                              inputId="barplot_features_input", 
                              label="Features",
                              choices=sort(unique(user_barplot_table$Feature)),
-                             selected = NULL,
+                             selected = sort(unique(user_barplot_table$Feature)),
                              multiple = TRUE,
                              selectize = TRUE,
                              width = NULL,
@@ -144,6 +144,10 @@ server <- function(input, output,session) {
     if (!is.null(query[['file']])) {
       query[['file']]
       user_barplot_table <- loading_and_preparing_ologram_table_barplot(query[['file']])
+      updateSelectInput(session, "barplot_features_input",
+                        choices=sort(unique(user_barplot_table$Feature)),
+                        selected=sort(unique(user_barplot_table$Feature))
+                        )
 
       user_barplot_table
     }
