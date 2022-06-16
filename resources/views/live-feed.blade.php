6b4ac197df37c0ec61460e87c577e1b383ff7055
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2022-06-16 07:00:30 +0200

    loading css and js from local files

--- resources/views/live-feed.blade.php
+++ resources/views/live-feed.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href={{ asset("css/bootstrap-5.1.3.min.css") }}>
 
     <title>live-feed</title>
 </head>
@@ -54,16 +53,13 @@
         </div>
     </div>
     
+    <script type="text/javascript" src={{ asset("js/bootstrap-5.1.3.min.js") }} ></script>
+    <script type="text/javascript" src={{ asset("js/jquery-3.6.0.min.js") }}></script>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  
     <script type = "text/JavaScript">
-        <!--
-           function AutoRefresh( t ) {
-              setTimeout("location.reload(true);", t);
-           }
-        //-->
+        function AutoRefresh( t ) {
+            setTimeout("location.reload(true);", t);
+        }
     </script>
     
 </body>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    
+    <link rel="stylesheet" type="text/css" href={{ asset("css/bootstrap-5.1.3.min.css") }}>
     
     <title>OLOGRAM test</title>
 
@@ -717,9 +717,10 @@
     </div>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  
+
+  <script type="text/javascript" src={{ asset("js/bootstrap-5.1.3.min.js") }} ></script>
+  <script type="text/javascript" src={{ asset("js/jquery-3.6.0.min.js") }}></script>
+
   <script type="text/javascript">
     $(document).ready(function (e) {
       $.ajaxSetup({
@@ -746,7 +747,6 @@
             }).done(function(response) {$this.unbind('submit').submit();});
         }); 
       });
-    </script>
 
 </body>
 </html>
